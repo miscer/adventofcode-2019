fc54b3eacfec1efcaeae7d9fcf2a368724bed54f
Michal Miškerník <michal@miskernik.sk>
2019-12-16 09:27:40 +0100

    Day 6, part two

--- 6/orbits.py
+++ 6/orbits.py
@@ -17,11 +17,29 @@ def create_graph(orbits):
     return graph
 
 
-def count_orbits(graph, key, depth=0):
-    return depth + sum(count_orbits(graph, child, depth + 1) for child in graph[key])
+def find_path(graph, start_key, target_key):
+    if start_key == target_key:
+        return [start_key]
+
+    for child_key in graph[start_key]:
+        path = find_path(graph, child_key, target_key)
+
+        if path is not None:
+            return [start_key] + path
+
+    return None
+
+
+def count_transfers(path_a, path_b):
+    for i in range(min(len(path_a), len(path_b))):
+        if path_a[i] != path_b[i]:
+            return len(path_a) + len(path_b) - 2 * (i + 1)
 
 
 orbits = parse_input(sys.stdin)
 graph = create_graph(orbits)
 
-print(count_orbits(graph, 'COM'))
+path_a = find_path(graph, 'COM', 'YOU')
+path_b = find_path(graph, 'COM', 'SAN')
+
+print(count_transfers(path_a, path_b))
