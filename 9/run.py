6d5c1cb1966cf26cd1aa91d7742f74cfc104c0c9
Michal Miškerník <michal@miskernik.sk>
2019-12-17 09:38:54 +0100

    Day 9, part two

--- 9/run.py
+++ 9/run.py
@@ -3,6 +3,6 @@ import sys
 import computer
 
 program = computer.parse_program(sys.stdin.readline().strip())
-output = computer.run_list(program, [1])
+output = computer.run_list(program, [2])
 print(output)
 
