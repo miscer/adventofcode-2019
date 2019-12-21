d5e579858abc85178ee20268c6fe66c97ea70896
Michal Miškerník <michal@miskernik.sk>
2019-12-21 17:37:50 +0100

    Day 11, part two

--- 11/robot.py
+++ 11/robot.py
@@ -19,6 +19,7 @@ def run_robot(program):
     direction = DIRECTION_UP
     position = (0, 0)
 
+    panels[position] = COLOR_WHITE
     robot = computer.run_generator(program)
 
     try:
@@ -50,6 +51,34 @@ def run_robot(program):
     return panels
 
 
+PRINT_COLORS = {
+    COLOR_BLACK: '.',
+    COLOR_WHITE: '#'
+}
+
+
+def print_panels(panels):
+    min_x, max_x, min_y, max_y = 0, 0, 0, 0
+
+    for position in panels.keys():
+        x, y = position
+
+        if x < min_x:
+            min_x = x
+        if x > max_x:
+            max_x = x
+        if y < min_y:
+            min_y = y
+        if y > max_y:
+            max_y = y
+
+    for y in range(max_y, min_y - 1, -1):
+        for x in range(min_x, max_x + 1):
+            color = panels[(x, y)]
+            print(PRINT_COLORS[color], end='')
+        print()
+
+
 program = computer.parse_program(sys.stdin.readline().strip())
 result = run_robot(program)
-print(len(result))
+print_panels(result)
