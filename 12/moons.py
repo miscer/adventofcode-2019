52425073a7c984e5ad0318653772bce222c7885b
Michal Miškerník <michal@miskernik.sk>
2019-12-23 07:52:13 +0100

    Day 12, part two

--- 12/moons.py
+++ 12/moons.py
@@ -1,4 +1,8 @@
+import copy
+import functools
 import itertools
+import math
+import operator
 
 import numpy as np
 
@@ -11,11 +15,17 @@ class Moon:
     def __repr__(self):
         return f'Moon({self.position}, {self.velocity})'
 
+    def __eq__(self, other):
+        return (self.position == other.position).all() and (self.velocity == other.velocity).all()
+
     def add_velocity(self, delta):
         self.velocity += delta
 
     def move_step(self):
         self.position += self.velocity
+
+    def coord_equal(self, other, i):
+        return self.position[i] == other.position[i] and self.velocity[i] == other.velocity[i]
 
 
 def apply_gravity(m1, m2):
@@ -41,27 +51,26 @@ def simulate_step(moons):
         m.move_step()
 
 
-def simulate(moons, steps):
-    print('Initial state:')
+def simulate(moons):
+    reference = copy.deepcopy(moons)
+    coord_steps = {}
+    total_steps = 1
 
-    for m in moons:
-        print(m)
+    simulate_step(moons)
 
-    print()
+    while len(coord_steps) < 3:
+        for i in range(3):
+            if all(m1.coord_equal(m2, i) for m1, m2 in zip(reference, moons)):
+                coord_steps.setdefault(i, total_steps)
 
-    for i in range(steps):
         simulate_step(moons)
+        total_steps += 1
 
-        print(f'After {i + 1} steps:')
-
-        for m in moons:
-            print(m)
-
-        print()
+    return coord_steps
 
 
-def total_energy(moons):
-    return sum(sum(abs(m.position) * sum(abs(m.velocity))) for m in moons)
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
 
 
 input_moons = [
@@ -78,6 +87,6 @@ test_moons = [
     Moon((3, 5, -1))
 ]
 
-simulate(input_moons, 1000)
-energy = total_energy(input_moons)
-print(f'Total energy: {energy}')
+steps = simulate(input_moons)
+steps_lcm = functools.reduce(lcm, steps.values())
+print(f'{steps_lcm} steps')
